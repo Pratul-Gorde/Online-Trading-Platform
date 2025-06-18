52463b8d0be3bd64abc06237c8802e73001c65c3
Pratul Gorde <pratulgorde87@gmail.com>
2025-06-18 18:16:07 +0530

    Add files via upload

---
+++ Dashboard/src/components/Dashboard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {useEffect,useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import Apps from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
+
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const token = Cookies.get("authToken");
+  console.log("Token from Cookies:", token);
+
+  useEffect(() => {
+    if (!token) {
+      alert("Unauthorized! Redirecting to login...");
+      window.location.href = "http://localhost:3000/login"; 
+    }
+  }, [token]);
+
+  return (
+    <div className="dashboard-container">
+      <GeneralContextProvider>
+        <WatchList />
+      </GeneralContextProvider>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
