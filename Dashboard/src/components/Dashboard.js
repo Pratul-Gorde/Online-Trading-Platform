4642a6e096554c31cffdd4ec68081138ab44b1dc
Pratul Gorde <pratulgorde87@gmail.com>
2025-07-25 23:33:05 +0530

    Update Dashboard.js

--- Dashboard/src/components/Dashboard.js
+++ Dashboard/src/components/Dashboard.js
@@ -21,7 +21,7 @@ const Dashboard = () => {
   useEffect(() => {
     if (!token) {
       alert("Unauthorized! Redirecting to login...");
-      window.location.href = "https://online-trading-platform-frontend.onrender.com"; 
+      window.location.href = "https://online-trading-platform-frontend.onrender.com/login"; 
     }
   }, [token]);
 
