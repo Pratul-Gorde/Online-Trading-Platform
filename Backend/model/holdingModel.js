1ccf5a0e645b5d6cddf68797ce6836ffbb266218
Pratul Gorde <pratulgorde87@gmail.com>
2025-07-26 19:43:14 +0530

    Update holdingModel.js

--- Backend/model/holdingModel.js
+++ Backend/model/holdingModel.js
@@ -2,6 +2,6 @@ const {model} = require("mongoose");
 
 const holdingSchema = require('../schemas/holdingSchema');
 
-const holdingModel = model("holding",holdingSchema);
+const holdingModel = model("holding",holdingSchema,"holdings");
 
 module.exports = holdingModel;
